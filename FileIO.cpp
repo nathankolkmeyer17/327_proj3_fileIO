b5528ee3a105884cc1738cb55b1419cf8a2ffa76
nathankolkmeyer17 <nathan.kolkmeyer.17@cnu.edu>
2019-11-02 11:52:45 -0700

    fileIO class

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,19 +6,43 @@
  */
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 
+
+vector<string>::iterator vectorIterator;
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	fstream openfile;
+	openfile.open(filename.c_str());
+	if(openfile.is_open()) {
+		while(!openfile.eof()){
+			string line;
+			getline(openfile, line);
+			contents += line;
+		}
+		openfile.close();
+		return SUCCESS;
+	}
+	contents.clear();
+	return COULD_NOT_OPEN_FILE_TO_READ;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	fstream outputfile;
+	outputfile.open(filename.c_str());
+	if(outputfile.is_open()) {
+		for(vectorIterator = myEntryVector.begin(); vectorIterator != myEntryVector.end(); ++vectorIterator) {
+			outputfile << *vectorIterator << endl;
+		}
+		outputfile.close();
+		return SUCCESS;
+	}
+	return COULD_NOT_OPEN_FILE_TO_WRITE;
 }
 
 
